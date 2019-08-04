b48af84c4de46aac10baceab09a4151d10c647eb
ajingbo <amajingbo@163.com>
2019-08-04 21:17:15 +0800

    登录功能完成

--- src/router/index.js
+++ src/router/index.js
@@ -8,9 +8,36 @@ import Home from '@/components/home/Home'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
   routes: [
     { path: '/login', component: Login },
     { path: '/home', component: Home }
   ]
 })
+// 全局导航守卫
+// 所有的路由都会先走守卫，添加导航守卫后，不管是访问那个路由，都会执行beforeEach回调函数中的代码
+// 因为所有的路由，都会经过该导航守卫，所以就可以在这个导航守卫的回调函数中
+// 判断有没有登录
+router.beforeEach((to, from, next) => {
+  if (to.path === '/login') {
+    // 如果访问的是login页面，直接放行，也就是任何人不管有没有登录
+    // 都可以访问登录页面
+    // 直接调用 next() 方法，表示：访问的是哪个页面，就展示这个页面的内容
+    next()
+  } else {
+    // 访问的不是登录页面
+
+    // 判断用户是否登录成功，
+    // 1 当用户登录成功，直接调用 next() 方法放行
+    // 2 当用户没有登录，应该调用 next('/login') 跳转到登录页面，让用户登录
+
+    // 通过登录成功时候保存的token，来作为有没有登录成功的条件
+    const token = localStorage.getItem('token')
+    if (token) {
+      next()
+    } else {
+      next('/login')
+    }
+  }
+})
+export default router
